d7e3d1546a29d15b1c793dc66524961c5ce45eba
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2025-03-17 17:26:04 -0500

    Se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,20 @@
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+
+function agregarAmigo() {
+    if (inputAmigo.value == ""){
+        alert("¡Alto! Debes ingresar un nombre")
+    }
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+};
+function sortearAmigo() {
+    const random = Math.floor((Math.random() * listaAmigos.length));
+    const amigoSecreto = listaAmigos [random];
+    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+};
+
+sortearAmigo();
+
